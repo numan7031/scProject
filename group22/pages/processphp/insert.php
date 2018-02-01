7852b8cd206b091f70e6865dbaf658bbb7741e15
numan7031 <numan7031@hotmail.com>
2018-02-01 23:20:45 +0700

    หน้าแสดงสถานที่ท่องเที่ยวได้50%ล่ะ

--- group22/pages/processphp/insert.php
+++ group22/pages/processphp/insert.php
@@ -1,10 +1,13 @@
 
 	<?php
 	// connect to the database
+	session_start();
+	ob_start();
 	$_review = $_REQUEST['rev'];
 	$_user = 1;
 	$_point = $_REQUEST['poin'];
-	$_attrac = $_REQUEST['att'];
+	$_attrac = $_SESSION['abc'];
+	date_default_timezone_set("Asia/Bangkok");
 	$_datenow = date("Y-m-d H:i:s");
 
 	if( !isset($_SESSION["userID"]) ){
@@ -24,11 +27,11 @@
 	//$_genderup = ucwords($_gender);
 	//echo $_genderup;
 
-	$sql = "INSERT INTO review(datereview,reviewdes, score, userID, attracID) VALUES ('$_datenow','$_review','$_point','$_user','$_attrac')";
+	$sql = "INSERT INTO review(datereview,reviewdes, score, userID, attracID) VALUES ('$_datenow','$_review',$_point,'$_user','$_attrac')";
 
 	if(mysqli_query($conn, $sql)){
 
-		echo "Insertion successfully.";
+		header("location:../actractionpage.php?id=".$_attrac."");
 
 	} else{
 
